1c455debe66622a59718761910ea766e8101ef67
Tal Tzfira <tal.tzfira@elastifile.com>
2016-01-24 14:46:45 +0200

    taltzf/test branch with user taltzf:

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ import "fmt"
 // Just some comment
 // Comment for branch testing
 // Comment for taltzf/test
+// Comment for taltzf/test as user taltzf
 
 func main() {
 	fmt.Printf("Hello, world.\n")
